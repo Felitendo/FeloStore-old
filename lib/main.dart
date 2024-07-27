20b7a60764618405909b3629230d281742bcd9fa
Felitendo <Felendo@web.de>
2024-07-27 19:59:29 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
-import 'dart:convert';
-import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:obtainium/pages/home.dart';
@@ -9,16 +8,17 @@ import 'package:obtainium/providers/logs_provider.dart';
 import 'package:obtainium/providers/native_provider.dart';
 import 'package:obtainium/providers/notifications_provider.dart';
 import 'package:obtainium/providers/settings_provider.dart';
+import 'package:obtainium/providers/source_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:provider/provider.dart';
 import 'package:dynamic_color/dynamic_color.dart';
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:background_fetch/background_fetch.dart';
 import 'package:easy_localization/easy_localization.dart';
-
-import 'package:flutter/services.dart' show rootBundle; // Specific import
-
-import 'package:obtainium/pages/apps.dart';
+// ignore: implementation_imports
+import 'package:easy_localization/src/easy_localization_controller.dart';
+// ignore: implementation_imports
+import 'package:easy_localization/src/localization.dart';
 
 List<MapEntry<Locale, String>> supportedLocales = const [
   MapEntry(Locale('en'), 'English'),
@@ -49,10 +49,28 @@ var fdroid = false;
 final globalNavigatorKey = GlobalKey<NavigatorState>();
 
 Future<void> loadTranslations() async {
-  // Initialize settings to get the forced locale if any.
+  // See easy_localization/issues/210
+  await EasyLocalizationController.initEasyLocation();
   var s = SettingsProvider();
   await s.initializeSettings();
   var forceLocale = s.forcedLocale;
+  final controller = EasyLocalizationController(
+    saveLocale: true,
+    forceLocale: forceLocale != null ? Locale(forceLocale) : null,
+    fallbackLocale: fallbackLocale,
+    supportedLocales: supportedLocales.map((e) => e.key).toList(),
+    assetLoader: const RootBundleAssetLoader(),
+    useOnlyLangCode: true,
+    useFallbackTranslations: true,
+    path: localeDir,
+    onLoadError: (FlutterError e) {
+      throw e;
+    },
+  );
+  await controller.loadTranslations();
+  Localization.load(controller.locale,
+      translations: controller.translations,
+      fallbackTranslations: controller.fallbackTranslations);
 }
 
 @pragma('vm:entry-point')
@@ -116,7 +134,6 @@ class _ObtainiumState extends State<Obtainium> {
   void initState() {
     super.initState();
     initPlatformState();
-    initFirstRun();
   }
 
   Future<void> initPlatformState() async {
@@ -140,59 +157,6 @@ class _ObtainiumState extends State<Obtainium> {
     if (!mounted) return;
   }
 
-  Future<void> initFirstRun() async {
-    SettingsProvider settingsProvider = context.read<SettingsProvider>();
-    bool isFirstRun = settingsProvider.checkAndFlipFirstRun();
-    if (isFirstRun) {
-      await loadImportConfig(context);
-    }
-  }
-
-  Future<void> loadImportConfig(BuildContext context) async {
-    try {
-      const configFilePath = 'assets/config.json';
-      String data = await rootBundle.loadString(configFilePath);
-      jsonDecode(data);
-      var appsProvider = context.read<AppsProvider>();
-      var settingsProvider = context.read<SettingsProvider>();
-      var value = await appsProvider.import(data);
-      var cats = settingsProvider.categories;
-      appsProvider.apps.forEach((key, value) {
-        for (var c in value.app.categories) {
-          if (!cats.containsKey(c)) {
-            cats[c] = generateRandomLightColor().value;
-          }
-        }
-      });
-      appsProvider.addMissingCategories(settingsProvider);
-      showMessage(
-        '${tr('importedX', args: [plural('apps', value.key.length)])}${value.value ? ' + ${tr('settings')}' : ''}',
-        context,
-      );
-    } catch (e) {
-      showError(e, context);
-    }
-  }
-
-  void showMessage(String message, BuildContext context) {
-    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(message)));
-  }
-
-  void showError(dynamic error, BuildContext context) {
-    ScaffoldMessenger.of(context)
-        .showSnackBar(SnackBar(content: Text('Error: $error')));
-  }
-
-  Color generateRandomLightColor() {
-    final random = Random();
-    return Color.fromRGBO(
-      200 + random.nextInt(55),
-      200 + random.nextInt(55),
-      200 + random.nextInt(55),
-      1,
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     SettingsProvider settingsProvider = context.watch<SettingsProvider>();
@@ -205,6 +169,7 @@ class _ObtainiumState extends State<Obtainium> {
       bool isFirstRun = settingsProvider.checkAndFlipFirstRun();
       if (isFirstRun) {
         logs.add('This is the first ever run of Obtainium.');
+        // If this is the first run, ask for notification permissions and add Obtainium to the Apps list
         Permission.notification.request();
         if (!fdroid) {
           getInstalledInfo(obtainiumId).then((value) {
@@ -245,6 +210,7 @@ class _ObtainiumState extends State<Obtainium> {
 
     return DynamicColorBuilder(
         builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
+      // Decide on a colour/brightness scheme based on OS and user settings
       ColorScheme lightColorScheme;
       ColorScheme darkColorScheme;
       if (lightDynamic != null &&
@@ -260,6 +226,7 @@ class _ObtainiumState extends State<Obtainium> {
             brightness: Brightness.dark);
       }
 
+      // set the background and surface colors to pure black in the amoled theme
       if (settingsProvider.useBlackTheme) {
         darkColorScheme =
             darkColorScheme.copyWith(surface: Colors.black).harmonized();
